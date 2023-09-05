d9e36531e5f028f1de3170aa4fd2929726dc06e8
sebas01999 <zebaz1999@outlook.es>
2023-09-05 11:37:06 -0500

    Navbar3

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -7,8 +7,10 @@ const Servicios = () => {
     const {servicios, servicio} = useServicios()
     const [open, setOpen] = useState(false)
     return (
-        <section id='seccion2'className='m-4 '>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  s gap-2'>
+        <section id='seccion2'className=''>
+
+            <h1 className='font-bold text-3xl text-center m-10'>Soluciones Unicas y Personalizadas</h1>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  s gap-2 mx-20'>
                 {
                     servicio ? (<Modal open={open} setOpen={setOpen}/>) : (<></>)
                 }
