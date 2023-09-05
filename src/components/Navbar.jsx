c0d3600d13b81cfbde4ca6bf8aab93448099fd99
JuanSalamanca19 <salamancajuandiego19@gmail.com>
2023-09-05 11:19:33 -0500

    Modificaciones banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white-900">
                 {item.name}
               </a>
             ))}
